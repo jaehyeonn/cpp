5549954d815644a9281d710babb53c92a76a8d7b
DESKTOP-2UEK7CH\KGA <a01049469494@gmail.com>
2023-05-30 18:02:52 +0900

    그냥 끄적인거

--- TestConsole001/TestConsole001/TestConsole001.cpp
+++ TestConsole001/TestConsole001/TestConsole001.cpp
@@ -2,19 +2,59 @@
 //
 
 #include <iostream>
-
-int main()
+#include <stdio.h> //전처리기 지시문 #include
+void MyNewPrint(int number)
 {
+    printf("입력 값은 %d 입니다. \n", number);
+}
+int PlusTwoNumbers(int number1,int number2)
+{
+    printf("입력 값은 %d, %d 입니다." ,number1, number2);
+    return number1 + number2;
+
+}
+int triple(int number1, int number2, int number3)
+{
+    printf("입력 값은 %d, %d, %d 입니다", number1, number2, number3);
+    return number1 * number2 * number3;
+}
+int main()
+
+{
+    int number = 10;
+
     std::cout << "Hello World!\n";
+
+    printf("세 수의 곱은 %d 입니다.\n\n\n", triple(123, 456, 789));
+
+    printf("Hello world!\"\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!%d\n", number - 2);
+
+    MyNewPrint(100);
+
+    printf("두 수의 합은 %d 입니다.\n\n", PlusTwoNumbers(5, 7));
+
+
+    // comment 주석 이 라인은 프로그램에 영향을 주지 않는다.
+    /*한줄 주석
+    전처리기 지시문 #include
+    <> 안에는 메모가 안됨
+    #include 안에 <> 안에 적어주는 것이 헤더파일
+    멤버 : 속해있는것
+    함수 (입력, 출력)
+    실제로 함수를 사용하는 줄에서 함수를 부른다. (Call)
+    retunr을 만나는 순간 함수는 종료한다 더 아래로 진행 안함
+    
+    */
+
+
+
+
+
+
+
+    return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
